a551bbcabb79d3f8729b5212596f971240b55d81
Clayman <Clayman@DESKTOP-DM8E77U>
2020-05-20 21:32:48 +0300

    Encapsulation.

--- IntroductionToOOP/IntroductionToOOP/main.cpp
+++ IntroductionToOOP/IntroductionToOOP/main.cpp
@@ -1,10 +1,44 @@
 #include<iostream>
 using namespace std;
 
+//	Inheritance
+//	Polymorphism
+
 class Point
 {
 	double x;
 	double y;
+public:
+	double get_x()const
+	{
+		return x;
+	}
+	double get_y()const
+	{
+		return y;
+	}
+	void set_x(double x)
+	{
+		this->x = x;
+	}
+	void set_y(double y)
+	{
+		this->y = y;
+	}
+};
+
+class Point3D :public Point
+{
+	double z;
+public:
+	double get_z() const
+	{
+		return z;
+	}
+	void set_z(double z)
+	{
+		this->z = z;
+	}
 };
 
 /*
@@ -20,12 +54,15 @@ void main()
 	Point A;//Объявление переменной 'A', типа 'Point'.
 			//Создание экземпляра 'A', структуры 'Point'.
 
-	A.x = 2;
-	A.y = 3;
-	cout << A.x << "\t" << A.y << endl;
+	A.set_x(2);
+	A.set_y(3);
+	cout << A.get_x() << "\t" << A.get_y() << endl;
 
 	Point* pA = &A;
-	cout << pA->x << "\t" << pA->y << endl;
+	cout << pA->get_x() << "\t" << pA->get_y() << endl;
+
+	Point3D B;
+	//B.
 }
 
 /*
