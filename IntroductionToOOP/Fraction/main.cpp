52a1f4de7ce440e2c25609dc50be2b6b012e637c
Clayman <Clayman@DESKTOP-DM8E77U>
2020-06-05 18:42:36 +0300

    Fraction start.

---
+++ IntroductionToOOP/Fraction/main.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+class Fraction
+{
+	int integer;	//Целая часть
+	int numerator;	//Числитель
+	int denominator;//Знаменатель
+public:
+	const int get_integer()const
+	{
+		return integer;
+	}
+	const int get_numerator()const
+	{
+		return numerator;
+	}
+	const int get_denominator()const
+	{
+		return denominator;
+	}
+	void set_integer(int integer)
+	{
+		this->integer = integer;
+	}
+	void set_numerator(int numerator)
+	{
+		this->numerator = numerator;
+	}
+	void set_denominator(int denominator)
+	{
+		this->denominator = denominator;
+	}
+};
+
+void main()
+{
+	
+}
