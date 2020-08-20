1c89dbc05234a0bac87ae9dc1f395391cb330a7c
okovtun <okovtun@itstep.org>
2020-08-20 20:59:15 +0300

    TODO: Iterator

--- DataContainers/ForwardList/main.cpp
+++ DataContainers/ForwardList/main.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
-using namespace std;
+//using namespace std;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::cerr;
 
 #define tab "\t"
 
@@ -15,12 +19,18 @@ public:
 	Element(int Data, Element* pNext = nullptr) :Data(Data), pNext(pNext)
 	{
 		count++;
+#ifdef DEBUG
 		cout << "EConstructor:\t" << this << endl;
+#endif // DEBUG
+
 	}
 	~Element()
 	{
 		count--;
+#ifdef DEBUG
 		cout << "EDestructor:\t" << this << endl;
+#endif // DEBUG
+
 	}
 	friend class ForwardList;
 };
@@ -32,23 +42,55 @@ class ForwardList
 	Element* Head;
 	int size;
 public:
+	int get_size()const
+	{
+		return this->size;
+	}
 	ForwardList()
 	{
 		this->Head = nullptr;
 		size = 0;
 		cout << "LConstructor:\t" << this << endl;
 	}
+	ForwardList(int size) :ForwardList()
+	{
+		while (size--)push_front(0);
+	}
+	ForwardList(const std::initializer_list<int>& il)
+	{
+		for (const int* it = il.begin(); it != il.end(); it++)
+		{
+			//*it = 5;
+			push_back(*it);
+		}
+	}
+	ForwardList(const ForwardList& other)
+	{
+		this->Head = nullptr;
+		this->size = 0;
+		for (Element* Temp = other.Head; Temp; Temp = Temp->pNext)push_back(Temp->Data);
+		cout << "LCopyConstructor:\t" << this << endl;
+	}
 	~ForwardList()
 	{
+		while (Head != nullptr)pop_front();
 		cout << "LDestructor:\t" << this << endl;
 	}
 
-	
+	//			Operators
+	int& operator[](int index)
+	{
+		if (index >= this->size)throw std::exception("Error: Out of range");
+		Element* Temp = Head;
+		for (int i = 0; i < index; i++)Temp = Temp->pNext;
+		return Temp->Data;
+	}
+
 	//			Добавление элементов
 	void push_front(int Data)
 	{
 		/*
-		Element* New = new Element(Data);	//Создаем новый элемент, и сохраняем в него добавляемое значение. 
+		Element* New = new Element(Data);	//Создаем новый элемент, и сохраняем в него добавляемое значение.
 		//Теперь этот элемент нужно ключить в список:
 		New->pNext = Head;
 		Head = New;*/
@@ -110,7 +152,7 @@ public:
 		Temp->pNext = nullptr;
 		size--;
 	}
-	
+
 	//			Methods:
 	void print()
 	{
@@ -122,9 +164,9 @@ public:
 		{
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 			Temp = Temp->pNext;//Переход на следующий элемент.
-		}
+}
 #endif // OLD_STYLE
-		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+		for (Element* Temp = Head; Temp; /*Temp = Temp->pNext*/Temp++)
 		{
 			cout << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
 		}
@@ -134,22 +176,25 @@ public:
 
 #define delimiter "\n----------------------------------\n"
 
-#define BASE_CHECK
+//#define BASE_CHECK
+//#define PREFORMANCE_CHECK
 
 void main()
 {
+	//int n;
+	//cout << "Input list size: "; cin >> n;
 #ifdef BASE_CHECK
-	int n;
-	cout << "Input list size: "; cin >> n;
 	ForwardList list;
 	for (int i = 0; i < n; i++)
 	{
 		list.push_front(rand() % 100);
 	}
-	list.push_back(123);
+	//list.push_back(123);
 	list.print();
 	cout << delimiter;
-	list.pop_front();
+	ForwardList list2 = list;
+	list2.print();
+	/*list.pop_front();
 	list.pop_back();
 	list.print();
 	int index;
@@ -157,9 +202,43 @@ void main()
 	cout << "Input index: "; cin >> index;
 	cout << "Input value: "; cin >> value;
 	list.insert(index, value);
-	list.print();
+	list.print();*/
 #endif // BASE_CHECK
 
+#ifdef PREFORMANCE_CHECK
+	ForwardList list(n);	//Создается список на 'n' элементов
+//list.print();
+
+	try
+	{
+		for (int i = 0; i < list.get_size(); i++)
+		{
+			list[i] = rand() % 100;
+		}
+		cout << "List loaded" << endl;
+		for (int i = 0; i < list.get_size(); i++)
+		{
+			//cout << list[i] << "\t";
+		}
+		cout << endl;
+		cout << "List printed" << endl;
+	}
+	catch (const std::exception& e)
+	{
+		cerr << e.what() << endl;
+	}
+#endif // PREFORMANCE_CHECK
+
+	//const int n = 5;
+	int arr[] = {3, 5, 8, 13, 21};
+	for (int i = 0; i < sizeof(arr)/sizeof(int); i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+
+	ForwardList list = { 3, 5, 8, 13, 21 };
+	list.print();
 	/*ForwardList list1;
 	list1.push_back(3);
 	list1.push_back(5);
